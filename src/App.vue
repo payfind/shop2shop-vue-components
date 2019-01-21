46a944d1ac1fd3e7eb95b5219ba6f9236e8c3f3e
Jeremy Walters <jeremy@ordercloud.co.za>
2019-01-21 14:06:15 +0200

    feat(Currency Input): Create new currency input
    Create new currency input component to be able to correctly format
    currency fields.

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,42 @@
 <template>
-  <v-app>
-    <v-content>
-      <S2SForm title="test Title">
-        <S2SCard>
-          <v-flex xs6>
-            <v-text-field name="name" label="label" id="id"></v-text-field>
-          </v-flex>
-          <v-flex xs6>
-            <v-text-field name="name" label="label" id="id"></v-text-field>
-          </v-flex>
-          <v-flex xs6>
-            <v-text-field name="name" label="label" id="id"></v-text-field>
-          </v-flex>
-          <v-flex xs6>
-            <v-text-field name="name" label="label" id="id"></v-text-field>
-          </v-flex>
-        </S2SCard>
-      </S2SForm>
-    </v-content>
-  </v-app>
+	<v-app>
+		<v-content>
+			<S2SForm title="test Title">
+				<S2SCard>
+					<v-flex xs6>
+						<S2SCurrencyInput label="Test Label"></S2SCurrencyInput>
+					</v-flex>
+					<v-flex xs6>
+						<v-text-field name="name" label="label" id="id"></v-text-field>
+					</v-flex>
+					<v-flex xs6>
+						<v-text-field name="name" label="label" id="id"></v-text-field>
+					</v-flex>
+					<v-flex xs6>
+						<v-text-field name="name" label="label" id="id"></v-text-field>
+					</v-flex>
+				</S2SCard>
+			</S2SForm>
+		</v-content>
+	</v-app>
 </template>
 
 <script>
 import S2SForm from "./components/S2SForm.vue";
 import S2SCard from "./components/S2SCard.vue";
+import S2SCurrencyInput from "./components/S2SCurrencyInput.vue";
 
 export default {
-  name: "App",
-  components: {
-    S2SCard,
-    S2SForm
-  },
-  data() {
-    return {
-      //
-    };
-  }
+	name: "App",
+	components: {
+		S2SCard,
+		S2SForm,
+		S2SCurrencyInput
+	},
+	data() {
+		return {
+			//
+		};
+	}
 };
 </script>
---
+++ src/components/S2SCurrencyInput.vue
@@ -0,0 +1,54 @@
+<template>
+	<v-text-field
+		name="amount"
+		v-model="displayValue"
+		:label="label"
+		@blur="isInputActive = false"
+		@focus="isInputActive = true"
+	></v-text-field>
+</template>
+
+<script lang="ts">
+import { Vue, Watch, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class S2SCurrencyInput extends Vue {
+	@Prop({ default: "R" })
+	symbol!: string;
+	@Prop({ default: " " })
+	thousandsSeperator!: string;
+	@Prop({ default: "No Label Provided" })
+	label!: string;
+
+	private value = 0;
+	private isInputActive = false;
+	private _displayValue = "";
+
+	get displayValue() {
+		debugger;
+		if (this.isInputActive) {
+			// Cursor is inside the input field. unformat display value for user
+			return this.value.toString();
+		} else {
+			// User is not modifying now. Format display value for user interface
+			return `${this.symbol} ` + (+this.value).toFixed(2).replace(/(\d)(?=(\d{3})+(?:\.\d+)?$)/g, `$1${this.thousandsSeperator}`);
+		}
+	}
+
+	set displayValue(modifiedValue: string) {
+		// Recalculate value after ignoring "R" and "," in user input
+		let newValue = parseFloat(modifiedValue.replace(/[^\d.]/g, ""));
+		// Ensure that it is not NaN
+		if (isNaN(newValue)) {
+			newValue = 0;
+		}
+		// Note: we cannot set this.value as it is a "prop". It needs to be passed to parent component
+		// $emit the event so that parent component gets it
+		this.value = newValue;
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
