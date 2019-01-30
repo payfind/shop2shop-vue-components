46f460c27f8a9e9ad9d6557df3ff5b59b2679bd3
Jeremy Walters <jeremy@ordercloud.co.za>
2019-01-30 10:56:48 +0200

    chore(Component Exports): Update exported comps

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,10 +1,12 @@
 import Vue from "vue";
 import S2SCard from "@/components/S2SCard.vue";
 import S2SForm from "@/components/S2SForm.vue";
+import S2SCurrencyInput from "@/components/S2SCurrencyInput.vue";
 
 const Components = {
   S2SForm,
-  S2SCard
+  S2SCard,
+  S2SCurrencyInput
 };
 
 Object.keys(Components).forEach(name => {
