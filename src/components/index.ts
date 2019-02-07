0d3ac6b24ea9205cf4238e755f17de7d2356d92e
Jeremy Walters <jeremy@ordercloud.co.za>
2019-02-07 10:41:51 +0200

    chore: Export form generator

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,11 +2,13 @@ import Vue from "vue";
 import S2SCard from "@/components/S2SCard.vue";
 import S2SForm from "@/components/S2SForm.vue";
 import S2SCurrencyInput from "@/components/S2SCurrencyInput.vue";
+import S2SFormGenerator from "@/components/formGenerator/S2SFormGenerator.vue";
 
 const Components = {
 	S2SForm,
 	S2SCard,
-	S2SCurrencyInput
+	S2SCurrencyInput,
+	S2SFormGenerator
 };
 
 Object.keys(Components).forEach(name => {
