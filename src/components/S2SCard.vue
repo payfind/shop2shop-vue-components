6f85cac9d4d1c9ef3570a6f56212c5c8f7c9974e
Jeremy Walters <jeremy@ordercloud.co.za>
2019-02-27 14:58:41 +0200

    refactor(S2SCard): General slot improvements

--- src/components/S2SCard.vue
+++ src/components/S2SCard.vue
@@ -1,14 +1,17 @@
 <template>
 	<v-card>
-		<v-toolbar light dense v-if="title">
+		<v-toolbar light v-if="title">
 			<v-toolbar-title>{{ title }}</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<slot name="toolbar-content"></slot>
 		</v-toolbar>
 		<v-sheet tile>
-			<v-card-text>
-				<div class="text-xs-center">
-					<slot></slot>
-				</div>
+			<v-card-text v-if="hasSlot('default')">
+				<slot></slot>
 			</v-card-text>
+
+			<!-- Removes padding for content. Typical usecase is using datagrid inside this component -->
+			<slot name="fluid"></slot>
 		</v-sheet>
 	</v-card>
 </template>
@@ -18,6 +21,10 @@ import { Vue, Watch, Component, Prop } from "vue-property-decorator";
 @Component
 export default class S2SCard extends Vue {
 	@Prop() title!: string;
+
+	hasSlot(name: string) {
+		return !!this.$slots[name] || !!this.$scopedSlots[name];
+	}
 }
 </script>
 
