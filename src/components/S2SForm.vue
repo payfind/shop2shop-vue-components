1a0accc3cc42afcd839e8173983b69e5b08d959c
Jeremy Walters <jeremy@ordercloud.co.za>
2019-04-05 12:13:27 +0200

    feat(S2SForm): Ability to have a filled-height container

--- src/components/S2SForm.vue
+++ src/components/S2SForm.vue
@@ -1,11 +1,11 @@
 <template>
 	<v-flex :fill-height="fillHeight">
-		<v-container ref="container" fluid elevation-0 grid-list-md text-xs-center :fill-height="fillHeight">
-			<v-toolbar ref="toolbar" :dark="dark" :light="light" :color="color" absolute>
-				<v-toolbar-title>{{ title }}</v-toolbar-title>
-				<v-spacer></v-spacer>
-				<slot name="toolbar-content"></slot>
-			</v-toolbar>
+		<v-toolbar v-if="!fillHeight" :dark="dark" :light="light" :color="color">
+			<v-toolbar-title>{{ title }}</v-toolbar-title>
+			<v-spacer></v-spacer>
+			<slot name="toolbar-content"></slot>
+		</v-toolbar>
+		<v-container ref="container" fluid elevation-0 grid-list-md text-xs-center :fill-height="fillHeight" :d-block="fillHeight">
 			<slot></slot>
 		</v-container>
 	</v-flex>
@@ -18,9 +18,10 @@ import { Vue, Watch, Component, Prop } from "vue-property-decorator";
 export default class S2SForm extends Vue {
 	@Prop() title!: string;
 	@Prop() color!: string;
-	@Prop() dark!: boolean;
-	@Prop() light!: boolean;
-	@Prop() fillHeight!: boolean;
+	@Prop(Boolean) dark!: boolean;
+	@Prop(Boolean) light!: boolean;
+	/** Currently you can have a toolbar with a form with filled height*/
+	@Prop(Boolean) fillHeight!: boolean;
 }
 </script>
 
